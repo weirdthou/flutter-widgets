2e849eb8b1dd3cb68a3090e972c1351d660823cf
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-10-03 19:29:17 +0600

    add drawer

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -18,6 +18,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter Widgets')),
         body: const Home(),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                ),
+                child: Text(
+                  'Drawer Title',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
