079078963f63f397697bc0ffdda398df977aec67
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-10-03 19:48:45 +0600

    format

--- flutter_widgets/lib/home.dart
+++ flutter_widgets/lib/home.dart
@@ -15,18 +15,19 @@ class Home extends StatelessWidget {
               return Container(
                 height: 200,
                 decoration: const BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        spreadRadius: 3,
-                      ),
-                    ],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    )),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
                 child: Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
